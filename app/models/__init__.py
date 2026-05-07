741a9d3c94951b8834643240329ea3b3e17192ba
AldanaRP <rissopatronaldana7@gmail.com>
2026-05-07 15:05:54 -0300

    solved foreign keys issues and inconsistencies in table names after renaming

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+from .project import Project
+from .project_task import Project_task
+from .status import Status
+from .task import Task
+from .work_session import Work_session
+
+print("IMPORTING MODELS")
--- app/models/project_task.py
+++ app/models/project_task.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, Date, ForeignKey
 from app.database.base import Base
 
-class project_task(Base):
+class Project_task(Base):
     __tablename__ ="project_tasks"
 
-    id = Column(Integer, ForeignKey("habits.id"), primary_key=True)
+    id = Column(Integer, ForeignKey("projects.id"), primary_key=True)
     date_completed = Column(Date, primary_key=True)
 
     status_id = Column(Integer, ForeignKey("statuses.id"), nullable=False)
