f88b1dd20a4563a9a2cf92a2c3c1112f675c1a85
AldanaRP <rissopatronaldana7@gmail.com>
2026-05-07 15:42:42 -0300

    modified connection.py to allow multiple devs

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://aldi:45949@localhost/personal_project"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
